a14b6e16bbcafcbfd457e4d41a8940b76ec652c6
alex-khrust <st.alex.ser@gmail.com>
2018-03-26 10:44:55 +0300

    расскоментировал валидатор формы

--- js/main.js
+++ js/main.js
@@ -6,115 +6,115 @@ $(window).on('load', function () {
     $preloader.delay(350).fadeOut('slow');
 });
 //---------- validator ------------------------
-// $(function(){
-//     $('#order__form').validate({
-//         rules: {
-//             name: {
-//                 required: true,
-//                 minlength: 2
-//             },
-//             phone: {
-//                 digits:true,
-//                 required: true
-//             },
-//             email: {
-//                 required: true,
-//                 email: true
-//             },
-//         },
-//         messages: {
-//             name: {
-//                 required: "Поле 'Имя' обязательно к заполнению",
-//                 minlength: "Введите не менее 2-х символов в поле 'Имя'"
-//             },
-//             email: {
-//                 required: "Поле 'Email' обязательно к заполнению",
-//                 email: "Необходим формат адреса email"
-//             },
-//             phone: {
-//                 required: "Поле 'Телефон' обязательно к заполнению",
-//                 digits: "Номер телефона должен содержать только цифры"
-//             }
-//         },
-//
-//     });
-// });
-// $(function(){
-//     $('#order__form--1').validate({
-//         rules: {
-//             name: {
-//                 required: true,
-//                 minlength: 2
-//             },
-//             phone: {
-//                 digits:true,
-//                 required: true
-//             },
-//             email: {
-//                 required: true,
-//                 email: true
-//             },
-//         },
-//         messages: {
-//             name: {
-//                 required: "Поле 'Имя' обязательно к заполнению",
-//                 minlength: "Введите не менее 2-х символов в поле 'Имя'"
-//             },
-//             email: {
-//                 required: "Поле 'Email' обязательно к заполнению",
-//                 email: "Необходим формат адреса email"
-//             },
-//             phone: {
-//                 required: "Поле 'Телефон' обязательно к заполнению",
-//                 digits: "Номер телефона должен содержать только цифры"
-//             }
-//         }
-//     });
-// });
-// $(function(){
-//     $('#order__form--2').validate({
-//         rules: {
-//             name: {
-//                 required: true,
-//                 minlength: 2
-//             },
-//             phone: {
-//                 digits:true,
-//                 required: true
-//             },
-//             email: {
-//                 required: true,
-//                 email: true
-//             },
-//         },
-//         messages: {
-//             name: {
-//                 required: "Поле 'Имя' обязательно к заполнению",
-//                 minlength: "Введите не менее 2-х символов в поле 'Имя'"
-//             },
-//             email: {
-//                 required: "Поле 'Email' обязательно к заполнению",
-//                 email: "Необходим формат адреса email"
-//             },
-//             phone: {
-//                 required: "Поле 'Телефон' обязательно к заполнению",
-//                 digits: "Номер телефона должен содержать только цифры"
-//             }
-//         }
-//     });
+$(function(){
+    $('#order__form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: {
+                digits:true,
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            name: {
+                required: "Поле 'Имя' обязательно к заполнению",
+                minlength: "Введите не менее 2-х символов в поле 'Имя'"
+            },
+            email: {
+                required: "Поле 'Email' обязательно к заполнению",
+                email: "Необходим формат адреса email"
+            },
+            phone: {
+                required: "Поле 'Телефон' обязательно к заполнению",
+                digits: "Номер телефона должен содержать только цифры"
+            }
+        },
+
+    });
+});
+$(function(){
+    $('#order__form--1').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: {
+                digits:true,
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            name: {
+                required: "Поле 'Имя' обязательно к заполнению",
+                minlength: "Введите не менее 2-х символов в поле 'Имя'"
+            },
+            email: {
+                required: "Поле 'Email' обязательно к заполнению",
+                email: "Необходим формат адреса email"
+            },
+            phone: {
+                required: "Поле 'Телефон' обязательно к заполнению",
+                digits: "Номер телефона должен содержать только цифры"
+            }
+        }
+    });
+});
+$(function(){
+    $('#order__form--2').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: {
+                digits:true,
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            name: {
+                required: "Поле 'Имя' обязательно к заполнению",
+                minlength: "Введите не менее 2-х символов в поле 'Имя'"
+            },
+            email: {
+                required: "Поле 'Email' обязательно к заполнению",
+                email: "Необходим формат адреса email"
+            },
+            phone: {
+                required: "Поле 'Телефон' обязательно к заполнению",
+                digits: "Номер телефона должен содержать только цифры"
+            }
+        }
+    });
 // });
 //=================================
-$('form').submit(function(e) {
-    var empty = $(this).parent().find("input").filter(function() {
-        return this.value === "";
-    });
-    if (!empty.length) {
-        //Если все графы заполнены, то показываем popup
-        $('.popup__thanks--inner').show();
-        //очищаем все данные текстовых полей, кроме кнопок
-        $('form input').not(':button, :submit').val('');
-    }
-    e.preventDefault();
+// $('form').submit(function(e) {
+//     var empty = $(this).parent().find("input").filter(function() {
+//         return this.value === "";
+//     });
+//     if (!empty.length) {
+//         //Если все графы заполнены, то показываем popup
+//         $('.popup__thanks--inner').show();
+//         //очищаем все данные текстовых полей, кроме кнопок
+//         $('form input').not(':button, :submit').val('');
+//     }
+//     e.preventDefault();
 });
 //=================================
 //------------ mmenu---------------------------------
